df274f94ec3fbc10053b7a539b49c2866e3467ad
KamranFaiz <kamranfaiz517@gmail.com>
2018-12-21 12:30:55 +0500

    assignment compeleted!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 @Component({
@@ -8,18 +8,34 @@ import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private fb: Facebook) {
+  constructor(public navCtrl: NavController, private fb: Facebook, public toastCtrl: ToastController) {
 
   }
 
 
   login(){
     this.fb.login(['public_profile', 'user_friends', 'email'])
-  .then((res: FacebookLoginResponse) => console.log('Logged into Facebook!', res))
-  .catch(e => console.log('Error logging into Facebook', e));
+  .then((res: FacebookLoginResponse) => {
+    console.log('Logged into Facebook!', res)
+    this.showToast('Logged into Facebook!')
+    this.navCtrl.push('LocationPage');
+  })
+  .catch(e => {
+     this.showToast('Error logging into Facebook');
+});
 
+  
+// this.fb.logEvent(this.fb.EVENTS.EVENT_NAME_ADDED_TO_CART);
+  }
 
-this.fb.logEvent(this.fb.EVENTS.EVENT_NAME_ADDED_TO_CART);
+  showToast(message: string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 2000,
+      position: 'middle'
+    });
+
+    toast.present(toast);
   }
 
 }
---
+++ src/pages/location/location.ts
@@ -0,0 +1,124 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  Marker,
+  Polyline,
+  GoogleMapsAnimation,
+  MyLocation,
+  LatLng
+} from '@ionic-native/google-maps';
+
+declare var google;
+
+/**
+ * Generated class for the LocationPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-location',
+  templateUrl: 'location.html',
+})
+export class LocationPage {
+
+  map: GoogleMap;
+   AIR_PORTS = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController, public toastCtrl: ToastController) {
+  }
+
+
+
+ 
+
+  
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    // Create a map after the view is loaded.
+    // (platform is already ready in app.component.ts)
+    this.map = GoogleMaps.create('map_canvas');
+    this.map.getMyLocation()
+    .then((location: MyLocation) => {
+      console.log(JSON.stringify(location, null ,2));
+
+      // Move the map camera to the location with animation
+      this.map.animateCamera({
+        target: location.latLng,
+        zoom: 17,
+        tilt: 30
+      })
+      .then(() => {
+        // add a marker
+        let marker: Marker = this.map.addMarkerSync({
+          position: location.latLng,
+          animation: GoogleMapsAnimation.BOUNCE
+        });
+
+        // show the infoWindow
+        marker.showInfoWindow();
+
+        // If clicked it, display the alert
+        marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+          this.showToast('clicked!');
+        });
+      });
+    });
+
+  }
+
+
+
+  showToast(message: string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 2000,
+      position: 'middle'
+    });
+
+    toast.present(toast);
+  }
+
+
+  drawline(){
+    
+    this.map.getMyLocation()
+    .then((location: MyLocation) => {
+    let HND_AIR_PORT = {lat: location.latLng.lat, lng: location.latLng.lng};
+    
+    this.AIR_PORTS.push(HND_AIR_PORT)
+    
+
+    this.map.animateCamera({target: this.AIR_PORTS});
+
+    let polyline: Polyline = this.map.addPolylineSync({
+      points: this.AIR_PORTS,
+      color: '#AA00FF',
+      width: 10,
+      geodesic: true,
+      clickable: true  // clickable = false in default
+    });
+
+    polyline.on(GoogleMapsEvent.POLYLINE_CLICK).subscribe((params: any) => {
+      let position: LatLng = <LatLng>params[0];
+
+      let marker: Marker = this.map.addMarkerSync({
+        position: position,
+        title: position.toUrlValue(),
+        disableAutoPan: true
+      });
+      marker.showInfoWindow();
+    });
+  });
+  }
+  
+}
